b53c3262c70ad670c627db12e2210821472c24d5
chronorose <rendemare@gmail.com>
2023-10-01 13:42:36 +0300

    fixed

--- bits.py
+++ bits.py
@@ -2,9 +2,14 @@ def setBitsCount(n):
     return recSetBitsCount(n, 0, True)
 
 
+def checkNeg(n, flag):
+    if n != -1 and n % 2 != 0:
+        return False
+    return flag
+
+
 def recSetBitsCount(n, count, flag):
-    if (n != -1 and n % 2 != 0):
-        flag = False
+    flag = checkNeg(n, flag)
     if n == 0:
         return count
     if n == -1:
