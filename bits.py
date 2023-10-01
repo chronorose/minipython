54855c8ea8714e5646898acf4774b134a60b4425
chronorose <rendemare@gmail.com>
2023-10-01 13:20:10 +0300

    fix

--- bits.py
+++ bits.py
@@ -1,26 +1,15 @@
-def isPow2Neg(n):
-    if n >= 0:
-        return False
-    while n != -1:
-        if n % 2 != 0:
-            return False
-        n = n // 2
-    return True
-
-
 def setBitsCount(n):
-    count = 0
-    if n == -1 or isPow2Neg(n):
-        return recSetBitsCount(n, count + 1)
-    return recSetBitsCount(n, count)
+    return recSetBitsCount(n, 0, True)
 
 
-def recSetBitsCount(n, count):
+def recSetBitsCount(n, count, flag):
+    if (n != -1 and n % 2 != 0):
+        flag = False
     if n == 0:
         return count
     if n == -1:
-        return count + 1
-    return recSetBitsCount(n >> 1, count + (n & 1))
+        return count + 1 + int(flag)
+    return recSetBitsCount(n >> 1, count + (n & 1), flag)
 
 
 n = int(input())
