0a869149fb9a71001a9d292ef9766f9e368fea6e
chronorose <rendemare@gmail.com>
2023-10-06 21:07:50 +0300

    something something fix

--- dict_swap.py
+++ dict_swap.py
@@ -2,7 +2,7 @@ a = {(1, 2): "a", (3, 4): "a", (5, 6): "a", 5: "a", 4: "a",
      6: "b", 9: "b", 10: "c"}
 
 
-def dict_swap_2(dict):
+def dict_swap(dict):
     new_dict = {}
     for key, val in dict.items():
         try:
@@ -17,23 +17,4 @@ def dict_swap_2(dict):
     return new_dict
 
 
-# def dict_swap(dict):
-#     new_dict = {}
-#     for i in dict.values():
-#         if i in new_dict:
-#             continue
-#         list = []
-#         for j in dict.keys():
-#             if dict[j] == i:
-#                 list.append(j)
-#         if len(list) == 1:
-#             new_dict[i] = list[0]
-#         else:
-#             new_entry = ()
-#             for j in list:
-#                 new_entry = new_entry + (j, )
-#             new_dict[i] = new_entry
-#     return new_dict
-
-
-print(dict_swap_2(a))
+print(dict_swap(a))
