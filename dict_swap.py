25998191351b133d1a9d13c741b5b8fc19a0c1e7
chronorose <rendemare@gmail.com>
2023-10-06 22:53:56 +0300

    skdjaksld

--- dict_swap.py
+++ dict_swap.py
@@ -17,7 +17,7 @@ def dict_swap_2(dict):
     return new_dict
 
 
-def dict_swap_3(dict):
+def dict_swap(dict):
     new_dict = {}
     uniq_holder = set()
     for key, val in dict.items():
@@ -32,5 +32,5 @@ def dict_swap_3(dict):
     return new_dict
 
 
+print(dict_swap(a))
 print(dict_swap_2(a))
-print(dict_swap_3(a))
