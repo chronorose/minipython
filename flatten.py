2cf631586e0859a71c8b103eea52418621d334d6
chronorose <rendemare@gmail.com>
2023-10-09 11:44:35 +0300

    sadqwe

--- flatten.py
+++ flatten.py
@@ -1,22 +1,23 @@
 a = [1, 2, 3, [1, 2], [3, [3, 4]]]
 
 
-def flattening(elem, new_list):
+def flattening(elem, new_list, depth):
     """
     recursively calls itself to append only not-lists to the new list
     """
-    if isinstance(elem, list):
+    if isinstance(elem, list) and depth != 0:
         for i in elem:
-            i = flattening(i, new_list)
+            i = flattening(i, new_list, depth - 1)
     else:
         new_list.append(elem)
 
 
-def flatten(list):
+def flatten(list, depth=-1):
     new_list = []
     for i in list:
-        i = flattening(i, new_list)
+        i = flattening(i, new_list, depth)
     return new_list
 
 
 print(flatten(a))
+print(flatten(a, 1))
