8d332c2d163856607b7521e7944b28ec60fe29d5
chronorose <rendemare@gmail.com>
2023-10-09 12:09:48 +0300

    flattening complete prob

--- flatten.py
+++ flatten.py
@@ -4,6 +4,7 @@ a = [1, 2, 3, [1, 2], [3, [3, 4]]]
 def flattening(elem, new_list, depth):
     """
     recursively calls itself to append only not-lists to the new list
+    or until depth is zero
     """
     if isinstance(elem, list) and depth != 0:
         for i in elem:
@@ -14,10 +15,12 @@ def flattening(elem, new_list, depth):
 
 def flatten(list, depth=-1):
     """
-    function that flattens incoming list with the specified depth
+    function that returns list made out of
+    list from input that is flattened with the specified depth
     (or until there are no sub-lists left)
     :param list: list to flatten
-    :param depth: depth of flattening
+    :param depth: depth of flattening, -1 by default,
+    flattens until no sub-lists left on any negative depths
     """
     new_list = []
     for i in list:
