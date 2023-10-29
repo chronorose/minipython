835dfc59aba53f017090a07e726ad9bf64918c2c
chronorose <rendemare@gmail.com>
2023-10-29 13:12:25 +0300

    палка

--- format_table.py
+++ format_table.py
@@ -1,24 +1,30 @@
+def str_generate(xs, indent, sep="", start=" ", finish=" ", палка="|"):
+    str = палка
+    if isinstance(xs, list):
+        for i in range(len(xs)):
+            str += f"{start}{xs[i]:{sep}<{indent[i]}}{finish}|"
+    else:
+        str += f"{start}{xs:{sep}<{indent}}{finish}|"
+    return str
+
+
 def format(cases, benchmarks, case_times):
     benchmarks.insert(0, "Benchmark")
-    indents = [0 for i in range(0, len(benchmarks) + 2)]
+    indents = [0 for i in range(0, len(benchmarks) + 1)]
     indents[0] = max(max(map(len, cases)), len(benchmarks[0]))
     for i in range(1, len(benchmarks)):
         indents[i] = len(benchmarks[i]) if len(benchmarks[i]) >= 4 else 4
-    first_str = "|"
-    for i in range(len(benchmarks)):
-        first_str += f" {benchmarks[i]:<{indents[i]}} |"
-    print(first_str)
-    str = ""
-    print(f"|{str:-<{sum(indents) + 3 * len(benchmarks) - 1}}|")
+    print(str_generate(benchmarks, indents))
+    print(str_generate(xs="", sep="-", start="", finish="",
+                       indent=(sum(indents) + 3 * len(benchmarks) - 1)))
     for i in range(1, len(cases) + 1):
-        str = ""
         for j in range(len(benchmarks)):
             if j == 0:
-                str += f"| {cases[i - 1]:<{indents[j]}} |"
+                string = str_generate(cases[i - 1], indents[j])
             else:
                 time = f"{case_times[i - 1][j - 1]:2}"
-                str += f" {time:<{indents[j]}} |"
-        print(str)
+                string += str_generate(time, indents[j], палка="")
+        print(string)
 
 
 format(["best case", "the worst case"],
