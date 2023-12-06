a6d314bea710e86c8e1885bbec083be4f30ff51e
chronorose <rendemare@gmail.com>
2023-12-06 19:16:24 +0300

    matplotlib is very fun hehe hihi :)

--- game_of_life.py
+++ game_of_life.py
@@ -1,5 +1,11 @@
 import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import time
+# 713 seconds for numpy
+# 251 seconds for default python
+
 
 def timer(f):
     def wrapper(*args, **kwargs):
@@ -15,7 +21,6 @@ def timer(f):
 @timer
 def gameOfLife(matrix):
     for iteration in range(128):
-        print(iteration)
         for i in range(size):
             for j in range(size):
                 sum = checkNeighbours(matrix, i, j)
@@ -23,6 +28,7 @@ def gameOfLife(matrix):
                     matrix[i][j] = 1
                 else:
                     matrix[i][j] = 0
+        plt.imshow(matrix)
 
 
 def checkNeighbours(matrix, i, j):
@@ -46,16 +52,38 @@ for i in range(size):
     row = [0 for i in range(size)]
     matrix.append(row)
 
+matrix[100][100] = 1
+matrix[100][101] = 1
+matrix[100][102] = 1
+matrix[101][102] = 1
+matrix[102][101] = 1
 n_matrix = []
 for i in range(size):
     n_row = [0 for i in range(size)]
     n_matrix.append(n_row)
+n_matrix[100][100] = 1
+n_matrix[100][101] = 1
+n_matrix[100][102] = 1
+n_matrix[101][102] = 1
+n_matrix[102][101] = 1
 n_matrix = np.array(n_matrix)
+
+
 gameOfLife(n_matrix)
 gameOfLife(matrix)
-endgame_sum = 0
+
+plt.axis('off')
+fig = plt.figure()
+anime = animation.FuncAnimation(fig, gameOfLife,
+                                interval=50, save_count=50)
+plt.show()
+anime()
+
+endgame_sum_python = 0
+endgame_sum_numpy = 0
 for i in range(size):
     for j in range(size):
-        endgame_sum += matrix[i][j]
+        endgame_sum_python += matrix[i][j]
+        endgame_sum_numpy += n_matrix[i][j]
 
-print(endgame_sum)
+print(endgame_sum_python == endgame_sum_numpy)
